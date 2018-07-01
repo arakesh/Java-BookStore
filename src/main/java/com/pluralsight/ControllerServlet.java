c117a6d16ac111d5b3aa15447cb27d0a19df2be1
rsingh <rsingh@localhost>
2018-07-01 14:15:07 -0700

    Delete BOOK changes

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -59,6 +59,9 @@ public class ControllerServlet extends HttpServlet {
 				case "/insert":
 					insertBook(request, response);
           break;
+				case "/delete":
+					deleteBook(request, response);
+          break;
         default:
 				   listBooks(request, response);
            break;
@@ -67,6 +70,20 @@ public class ControllerServlet extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void deleteBook(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+		int id = Integer.parseInt(request.getParameter("id"));
+		System.out.println("ID:->" +id);
+		bookDAO.deleteBook(id);
+		response.sendRedirect("list");
+		
+	}
+
+	private void editBook(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// TODO Auto-generated method stub
+	
 	}
 
 	private void showBookAdmin(HttpServletRequest request, HttpServletResponse response)
